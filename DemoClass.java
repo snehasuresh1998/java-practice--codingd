94fab40f63e85ffb69051c0bd5be164719722371
snehasuresh1998 <83752629+snehasuresh1998@users.noreply.github.com>
2022-02-17 10:41:11 +0530

    Add files via upload

---
+++ DemoClass.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.junit.runner.JUnitCore;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Result res= JUnitCore.runClasses();
+
+	}
+
+}
